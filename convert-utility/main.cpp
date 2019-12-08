f8e65e5b556a396e0bac8f1198dbe1abf27c1b33
DarkMorford <DarkMorford@users.noreply.github.com>
2019-12-08 10:28:05 -0800

    Apply patch from Trac ticket 12

--- convert-utility/main.cpp
+++ convert-utility/main.cpp
@@ -235,7 +235,15 @@ int32_t GetInputFormat(FILE * inputFile, AudioFormatDescription * theInputFormat
                         theInputFormat->mChannelsPerFrame = theReadBuffer[6];
                         theSampleRate = ((int32_t)(theReadBuffer[11]) << 24) + ((int32_t)(theReadBuffer[10]) << 16) + ((int32_t)(theReadBuffer[9]) << 8) + theReadBuffer[8];
                         theInputFormat->mSampleRate = theSampleRate;
-                        theInputFormat->mBitsPerChannel = theReadBuffer[18];
+                        // Check the bit depth of the WAV file; if it's 16 or 24, proceed.
+                        if (theReadBuffer[18] == 16 || theReadBuffer[18] == 24) {
+                            theInputFormat->mBitsPerChannel = theReadBuffer[18];
+                        }
+                        else {
+                            // If not, kill it dead.
+                            *theFileType = 0;
+                            return -1;
+                        }
                         theInputFormat->mFormatFlags = kALACFormatFlagIsSignedInteger | kALACFormatFlagIsPacked; // always little endian
                         theInputFormat->mBytesPerPacket = theInputFormat->mBytesPerFrame = (theInputFormat->mBitsPerChannel >> 3) * theInputFormat->mChannelsPerFrame;
                         theInputFormat->mFramesPerPacket = 1;
